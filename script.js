44740ecfcc251d045f55f9d3f0f25d2e6acc69b9
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-06-05 18:27:32 +0200

    creando script

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.tema').forEach((item)=>{
+    item.addEventListener('click',func{
+    })
+})
+
+
+const screen = document.querySelector('.screen');
+document.querySelectorAll('.bts').forEach((item)=>(
+    item.addEventListener('click', teclas)
+))
